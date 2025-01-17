d7147707bbb716306a45dbfb3ccf7e706d61b27b
Jad Bizri <jb4954@hotmail.com>
2025-01-17 12:18:01 -0500

    Added then, catch blocks for error handling

--- src/services/transfers.service.ts
+++ src/services/transfers.service.ts
@@ -1,79 +1,101 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { error } from 'console';
 import { Block, ethers, Log } from 'ethers';
 
 @Injectable()
 export class TransfersService {
-
   constructor(private configService: ConfigService) {}
 
-  private readonly url: string ='https://eth-mainnet.g.alchemy.com/v2/' + this.configService.get<string>('ALCHEMY_API_KEY');
+  private readonly url: string =
+    'https://eth-mainnet.g.alchemy.com/v2/' +
+    this.configService.get<string>('ALCHEMY_API_KEY');
   private provider = new ethers.JsonRpcProvider(this.url);
 
-  async fetchTransferLogs(startBlock: number, endBlock: number): Promise<Array<any>> {
+  async fetchTransferLogs(
+    startBlock: number,
+    endBlock: number,
+  ): Promise<Array<any>> {
     const event = ethers.id('Transfer(address,address,uint256)');
 
-    const logs = await this.provider.getLogs({
-      fromBlock: startBlock,
-      toBlock: endBlock,
-      topics: [event],
-    });
-
-    //filter out non erc-20 tokens and filter out what we need
-    return this.processLogs(logs);
+    return await this.provider
+      .getLogs({
+        fromBlock: startBlock,
+        toBlock: endBlock,
+        topics: [event],
+      })
+      .then((logs) => this.processLogs(logs))
+      .catch((error) => {
+        console.error('Error: ' + error);
+        throw error;
+      });
   }
 
+  //filter out non erc-20 tokens and filter out what we need
   processLogs(logs: Array<Log>): Array<any> {
     return logs
-    .filter((log) => log.topics.length === 3)
-    .map((log) => {
-      return {
-        transactionHash: ethers.stripZerosLeft(log.transactionHash),
-        blockNumber: log.blockNumber,
-        tokenAddress: log.address,
-        fromAddress: log.topics[1] ? ethers.stripZerosLeft(log.topics[1]) : null,
-        toAddress: log.topics[2] ? ethers.stripZerosLeft(log.topics[2]) : null,
-        amount: ethers.AbiCoder.defaultAbiCoder().decode(['uint256'], log.data).toString()
-      }
-    });
+      .filter((log) => log.topics.length === 3)
+      .map((log) => {
+        return {
+          transactionHash: ethers.stripZerosLeft(log.transactionHash),
+          blockNumber: log.blockNumber,
+          tokenAddress: log.address,
+          fromAddress: log.topics[1]
+            ? ethers.stripZerosLeft(log.topics[1])
+            : null,
+          toAddress: log.topics[2]
+            ? ethers.stripZerosLeft(log.topics[2])
+            : null,
+          amount: ethers.AbiCoder.defaultAbiCoder()
+            .decode(['uint256'], log.data)
+            .toString(),
+        };
+      });
   }
 
   async getLatestBlockNumber(): Promise<number> {
-    return await this.provider.getBlockNumber()
+    return await this.provider
+      .getBlockNumber()
+      .then((num) => num)
+      .catch((error) => {
+        console.error(error);
+        throw error;
+      });
   }
 
   //binary search function that gets the first block number created 3 months ago
   async getFirstBlock(): Promise<number> {
-    try {
-      const threeMonthsInSecs: number = 7776000;
+    const threeMonthsInSecs: number = 7776000;
+    let left = 0;
+    let right = await this.getLatestBlockNumber(); //latest block number
 
-      let left = 0;
-      let right = await this.provider.getBlockNumber(); //latest block number
-      const targetTimestamp =
-        (await this.getBlockTimestampByNumber(right)) - threeMonthsInSecs; //timestamp of 3 months ago
+    const targetTimestamp =
+      (await this.getBlockTimestampByNumber(right)) - threeMonthsInSecs; //timestamp of 3 months ago
+    let targetBlock: Block;
 
-      let targetBlock: Block;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      await this.provider
+        .send('eth_getBlockByNumber', ['0x' + mid.toString(16), false])
+        .then((block) => {
+          targetBlock = block;
+        })
+        .catch((error) => {
+          console.error('Error: ' + error);
+          throw error;
+        });
 
-      while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        targetBlock = await this.provider.send('eth_getBlockByNumber', [
-          '0x' + mid.toString(16),
-          false,
-        ]);
-        const midTimestamp = targetBlock.timestamp;
+      const midTimestamp = targetBlock.timestamp;
 
-        if (midTimestamp === targetTimestamp) {
-          return targetBlock.number;
-        }
-
-        if (midTimestamp < targetTimestamp) {
-          left = mid + 1;
-        } else right = mid - 1;
+      if (midTimestamp === targetTimestamp) {
+        return targetBlock.number;
       }
-      return targetBlock.number;
-    } catch (err) {
-      throw err;
+
+      if (midTimestamp < targetTimestamp) {
+        left = mid + 1;
+      } else right = mid - 1;
     }
+    return targetBlock.number;
   }
 
   async getBlockTimestampByNumber(blockNum: number): Promise<number> {
@@ -82,9 +104,9 @@ export class TransfersService {
       .then((block) => {
         return block.timestamp;
       })
-      .catch((err) => {
-        console.error('ERROR: ', err);
-        throw err;
+      .catch((error) => {
+        console.error('ERROR: ', error);
+        throw error;
       });
   }
 }
